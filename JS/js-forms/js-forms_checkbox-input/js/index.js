863264f4ddd924f6332be8296e2744b31b5f6915
Davide Esposito <davideesposito2911@gmail.com>
2024-03-07 18:22:57 +0100

    add solution bonus challenge

--- JS/js-forms/js-forms_checkbox-input/js/index.js
+++ JS/js-forms/js-forms_checkbox-input/js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const successText = document.querySelector(`[data-js="success"]`);
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -12,14 +13,25 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+function hideSuccessText() {
+  successText.setAttribute("hidden", "");
+}
+
+function showSuccessText() {
+  successText.removeAttribute("hidden");
+}
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
   if (tosCheckbox.checked) {
     hideTosError();
+    showSuccessText();
   } else {
-    return showTosError();
+    showTosError();
+    hideSuccessText();
+    return;
   }
   // --^-- write your code here --^--
 
@@ -30,7 +42,9 @@ form.addEventListener("submit", (event) => {
 tosCheckbox.addEventListener("change", () => {
   if (tosCheckbox.checked) {
     hideTosError();
+    hideSuccessText();
   } else {
     showTosError();
+    hideSuccessText();
   }
 });
