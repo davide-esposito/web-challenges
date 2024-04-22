50947e61633676ece28932e5a303f32124966364
Davide Esposito <davideesposito2911@gmail.com>
2024-04-22 17:56:25 +0200

    part3/recap

--- React/recap-projekt-4/color-theme-projekt/src/App.js
+++ React/recap-projekt-4/color-theme-projekt/src/App.js
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header.js";
 import Theme from "./components/Theme/Theme.js";
-import { themes } from "./db.js";
+import { themes as initialThemes } from "./db.js";
+import NewThemeForm from "./components/NewThemeForm/NewThemeForm.js";
+import { v4 as uuid } from "uuid";
 
 export default function App() {
+  const [themes, setThemes] = useState(initialThemes);
+
+  function handleAddTheme(newTheme) {
+    const newThemeWithId = { ...newTheme, id: uuid() };
+    setThemes([newThemeWithId, ...themes]);
+  }
+
   return (
     <>
       <header className="header">
         <Header />
       </header>
       <main className="main-container">
+        <NewThemeForm onSubmit={handleAddTheme} />
         {themes.map((theme) => (
           <Theme key={theme.id} theme={theme} name={theme.name} />
         ))}
---
+++ React/recap-projekt-4/color-theme-projekt/src/components/NewThemeForm/NewThemeForm.js
@@ -0,0 +1,66 @@
+import "./NewThemeForm.css";
+
+export default function NewThemeForm({ onSubmit }) {
+  const initialData = {
+    name: "",
+    colors: [
+      { role: "primary", value: "#6200ee" },
+      { role: "secondary", value: "#03dac6" },
+      { role: "surface", value: "#ffffff" },
+      { role: "surface-on", value: "#000000" },
+    ],
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData);
+
+    const newTheme = {
+      name: data.name,
+      colors: [
+        { role: "primary", value: data.primary },
+        { role: "secondary", value: data.secondary },
+        { role: "surface", value: data.surface },
+        { role: "surface-on", value: data["surface-on"] },
+      ],
+    };
+
+    onSubmit(newTheme);
+  }
+
+  return (
+    <form className="theme-form" onSubmit={handleSubmit}>
+      <h2 className="theme-form__title">Add new Theme</h2>
+      <div className="theme-form__name-input">
+        <label htmlFor="name">Theme Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Enter Theme-Name"
+          required
+        />
+      </div>
+      <fieldset className="theme-form__color-inputs">
+        {initialData.colors.map((color) => (
+          <div>
+            <label htmlFor={color.role}>{color.role}:</label>
+
+            <input
+              key={color.role}
+              className="theme-form__color-input"
+              type="color"
+              name={color.role}
+              defaultValue={color.value}
+            />
+          </div>
+        ))}
+      </fieldset>
+      <button className="theme-form__submit-button" type="submit">
+        Add Theme
+      </button>
+    </form>
+  );
+}
