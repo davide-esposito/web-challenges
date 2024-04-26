98acf0ae283e483621d4f30f9c55161ac7692e96
Davide Esposito <davideesposito2911@gmail.com>
2024-04-26 10:57:14 +0200

    add part6/recap

--- React/recap-projekt-4/color-theme-projekt/src/App.js
+++ React/recap-projekt-4/color-theme-projekt/src/App.js
@@ -2,23 +2,47 @@ import "./App.css";
 import Header from "./components/Header/Header.js";
 import Theme from "./components/Theme/Theme.js";
 import { themes as initialThemes } from "./db.js";
-import NewThemeForm from "./components/NewThemeForm/NewThemeForm.js";
+import NewThemeForm from "./components/ThemeForm/ThemeForm.js";
 import { v4 as uuid } from "uuid";
 import useLocalStorageState from "use-local-storage-state";
+import { useEffect } from "react";
 
 export default function App() {
   const [themes, setThemes] = useLocalStorageState("Themes", {
     defaultValue: initialThemes,
   });
 
+  useEffect(() => {
+    console.log("Themes updated:", themes);
+  }, [themes]);
+
   function handleAddTheme(newTheme) {
     const newThemeWithId = { ...newTheme, id: uuid() };
-    setThemes([newThemeWithId, ...themes]);
+    const newThemes = [newThemeWithId, ...themes];
+    console.log("Adding new theme:", newThemeWithId);
+    setThemes(newThemes);
+    console.log("Updated themes list after adding:", newThemes);
   }
 
   function handleDeleteTheme(id) {
-    const modifiedThemes = themes.filter((theme) => theme.id !== id);
+    console.log("Deleting theme with ID:", id);
+    const updatedThemes = themes.filter((theme) => theme.id !== id);
+    console.log("Updated themes list after deletion:", updatedThemes);
+    setThemes(updatedThemes);
+  }
+
+  function handleEditTheme(updatedTheme) {
+    console.log("Editing theme:", updatedTheme);
+
+    const modifiedThemes = themes.map((theme) => {
+      if (theme.id === updatedTheme.id) {
+        return updatedTheme;
+      }
+      return theme;
+    });
+    console.log("Current themes before update:", themes);
     setThemes(modifiedThemes);
+    console.log("Current themes after update:", modifiedThemes);
   }
 
   return (
@@ -27,13 +51,13 @@ export default function App() {
         <Header />
       </header>
       <main className="main-container">
-        <NewThemeForm onSubmit={handleAddTheme} />
+        <NewThemeForm onAdd={handleAddTheme} />
         {themes.map((theme) => (
           <Theme
             key={theme.id}
             theme={theme}
-            name={theme.name}
             onDelete={handleDeleteTheme}
+            onEdit={handleEditTheme}
           />
         ))}
       </main>
--- React/recap-projekt-4/color-theme-projekt/src/components/ThemeDetail/ThemeDetail.js
+++ React/recap-projekt-4/color-theme-projekt/src/components/ThemeDetail/ThemeDetail.js
@@ -1,22 +1,50 @@
 import "./ThemeDetail.css";
 import ColorCard from "../ColorCard/ColorCard";
+import { useState } from "react";
+import NewThemeForm from "../ThemeForm/ThemeForm";
 
-export default function ThemeDetail({ theme, onDelete }) {
+export default function ThemeDetail({ theme, onDelete, onEdit }) {
+  const [displayState, setDisplayState] = useState("Detail");
+
+  function handleEditView() {
+    setDisplayState("Edit");
+  }
+
   return (
-    <section className="theme-detail">
-      <div className="color-cards">
-        {" "}
-        {theme.colors.map((color, index) => (
-          <ColorCard key={index} color={color} />
-        ))}
-      </div>
-      <button
-        onClick={() => onDelete(theme.id)}
-        className="detail-theme__delete-button"
-        type="button"
-      >
-        Delete Theme
-      </button>
-    </section>
+    <>
+      {displayState === "Detail" && (
+        <section className="theme-detail">
+          <div className="color-cards">
+            {theme.colors.map((color, index) => (
+              <ColorCard key={color.role} color={color} />
+            ))}
+          </div>
+          <button
+            onClick={() => onDelete(theme.id)}
+            className="detail-theme__delete-button"
+            type="button"
+          >
+            Delete Theme
+          </button>
+          <button
+            className="detail-theme__edit-button"
+            type="button"
+            onClick={handleEditView}
+          >
+            Edit Theme
+          </button>
+        </section>
+      )}
+      {displayState === "Edit" && (
+        <NewThemeForm
+          isEditMode="true"
+          initialData={theme}
+          onEdit={(theme) => {
+            onEdit(theme);
+            setDisplayState("Detail");
+          }}
+        />
+      )}
+    </>
   );
 }
